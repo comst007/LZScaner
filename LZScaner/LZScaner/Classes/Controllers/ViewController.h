95fd4d1c6ff7fe94d01e8892c132cfa646ba2028
LiuZHao <comst@comstdeMacBook-Pro.local>
2016-04-05 16:29:37 +0800

    init

---
+++ LZScaner/LZScaner/Classes/Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZScaner
+//
+//  Created by comst on 16/4/5.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
